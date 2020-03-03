77034caf155ad2962808839d89643ab50370dd64
tearth <tearthdev@gmail.com>
2020-03-03 22:49:44 +0100

    Add example app

---
+++ ExampleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using DotNetLaunchDashboard;
+
+namespace ExampleApp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var launchDashboardCore = new LaunchDashboardCore();
+            var result1 = await launchDashboardCore.Analysed("spacex").WithFlightNumber(80).ExecuteAsync();
+            var result2 = await launchDashboardCore.Events("spacex").WithFlightNumber(80).ExecuteAsync();
+            var result3 = await launchDashboardCore.Launches("spacex").WithFlightNumber(80).ExecuteAsync();
+            var result4 = await launchDashboardCore.Raw("spacex").WithFlightNumber(80).ExecuteAsync();
+
+            Console.Read();
+        }
+    }
+}
