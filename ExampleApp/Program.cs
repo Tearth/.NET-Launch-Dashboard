d0402925a15fba65931ef6ddcc8e3efbd57bcee5
tearth <tearthdev@gmail.com>
2020-03-20 13:18:09 +0100

    Add live telemetry to the example app

--- ExampleApp/Program.cs
+++ ExampleApp/Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using System.Threading.Tasks;
 using DotNetLaunchDashboard;
+using DotNetLaunchDashboard.Live;
 
 namespace ExampleApp
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static async Task Main()
         {
+            var liveTelemetry = new LiveTelemetry();
+            liveTelemetry.OnConnected += (sender, args) => Console.WriteLine("Connected to the live telemetry stream");
+            liveTelemetry.OnClosed += (sender, args) => Console.WriteLine($"Disconnected from the live telemetry stream (reason: {args})");
+            liveTelemetry.OnError += (sender, args) => Console.WriteLine($"Error: {args.Text}");
+            liveTelemetry.OnRawTelemetry += (sender, args) =>
+                Console.WriteLine($"Raw: time: {args.Time}, " +
+                                  $"alt: {args.Altitude}, " +
+                                  $"vel: {args.Velocity}");
+            liveTelemetry.OnAnalysedTelemetry += (sender, args) =>
+                Console.WriteLine($"Analysed: time: {args.Time}, " +
+                                  $"alt: {args.Altitude}, " +
+                                  $"vel: {args.Velocity}, " +
+                                  $"velX: {args.VelocityX}, " +
+                                  $"velY: {args.VelocityY}, " +
+                                  $"dDist: {args.DownrangeDistance}, " +
+                                  $"angle: {args.Angle}, " +
+                                  $"q: {args.Q}");
+            liveTelemetry.Start();
+            
             var launchDashboardCore = new LaunchDashboardCore();
             var result1 = await launchDashboardCore.Info().ExecuteAsync();
             var result2 = await launchDashboardCore.Analysed("spacex").WithFlightNumber(80).ExecuteAsync();
