7ad8aa4b466b39b914f1301ea3ddc1f8e034940c
tearth <tearthdev@gmail.com>
2020-03-20 13:17:55 +0100

    Add comments to the live telemetry class

--- DotNetLaunchDashboard/Live/LiveTelemetry.cs
+++ DotNetLaunchDashboard/Live/LiveTelemetry.cs
@@ -6,16 +6,41 @@ using SocketIOClient.Arguments;
 
 namespace DotNetLaunchDashboard.Live
 {
+    /// <summary>
+    /// Core of the live telemetry, use this class to receive data in real time.
+    /// </summary>
     public class LiveTelemetry : IDisposable
     {
+        /// <summary>
+        /// An event fired when the client has been connected to the server.
+        /// </summary>
         public event EventHandler<EventArgs> OnConnected;
+
+        /// <summary>
+        /// An event fired when the client has closed connection with the server (intentionally or not).
+        /// </summary>
         public event EventHandler<ServerCloseReason> OnClosed;
+
+        /// <summary>
+        /// An event fired when there was some error during communication between the client and server.
+        /// </summary>
         public event EventHandler<ResponseArgs> OnError;
+
+        /// <summary>
+        /// An event fired when the client has received a chunk of the raw telemetry.
+        /// </summary>
         public event EventHandler<TelemetryChunkModel> OnRawTelemetry;
+
+        /// <summary>
+        /// An event fired when the client has received a chunk of the analysed telemetry.
+        /// </summary>
         public event EventHandler<AnalysedTelemetryChunkModel> OnAnalysedTelemetry;
 
         private readonly SocketIO _socket;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="LiveTelemetry"/> class.
+        /// </summary>
         public LiveTelemetry()
         {
             _socket = new SocketIO(Configuration.BaseApiAddress);
@@ -27,12 +52,19 @@ namespace DotNetLaunchDashboard.Live
             _socket.On("analysed", HandleAnalysedTelemetry);
         }
 
+        /// <summary>
+        /// Connects to the server and initializes communication. After complete call, you should start receiving telemetry from
+        /// the server. If you want to stop it, use <see cref="Stop"/> method.
+        /// </summary>
         public async void Start()
         {
             await _socket.ConnectAsync();
             await _socket.EmitAsync("join", (object) new[] {"raw", "analysed"});
         }
 
+        /// <summary>
+        /// Disconnects from the server. You won't receive any telemetry after call. If you want to resume, use <see cref="Start"/> method.
+        /// </summary>
         public async void Stop()
         {
             await _socket.CloseAsync();
@@ -54,9 +86,9 @@ namespace DotNetLaunchDashboard.Live
             OnClosed?.Invoke(this, reason);
         }
 
-        private void SocketOnError(ResponseArgs obj)
+        private void SocketOnError(ResponseArgs args)
         {
-            OnError?.Invoke(this, obj);
+            OnError?.Invoke(this, args);
         }
 
         private void HandleRawTelemetry(ResponseArgs args)
